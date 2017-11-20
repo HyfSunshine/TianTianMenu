46c60752cea2cdb50e3495239f208994470ab73c
丨Rainbow丨 <775183940@qq.com>
2017-11-20 10:46:42 +0800

    init

---
+++ app/src/main/java/com/tiantian/menu/MainActivity.java
@@ -0,0 +1,22 @@
+package com.tiantian.menu;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.hello_world)
+    TextView mTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        mTextView.setText(R.string.app_name);
+    }
+}
